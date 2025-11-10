2433c8150fdf36039604e2cba985ce1f7139038f
lhy <lhy30919@naver.com>
2025-11-10 18:07:10 +0900

    for while

---
+++ 04/02_반복문/woodcutter.py
@@ -0,0 +1,14 @@
+treehit = 0
+
+### while ###
+while treehit < 10:
+    treehit += 1
+    print("나무를 %d번 찍었습니다." % treehit)
+    if treehit == 10:
+        print("나무가 넘어갑니다.")
+
+### for ###
+# for treehit in range(1, 11):
+#     print("나무를 %d번 찍었습니다." % treehit)
+#     if treehit == 10:
+#         print("나무가 넘어갑니다.")
