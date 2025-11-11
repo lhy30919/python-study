7fbba981bd4fd44d3477c794939c85d4f0a66f02
lhy <lhy30919@naver.com>
2025-11-11 19:50:54 +0900

    json

--- 06/02_json/jsonfile_python_jsonstr.py
+++ 06/02_json/jsonfile_python_jsonstr.py
@@ -4,3 +4,10 @@ fd = open("seoul.json", "r")
 data = json.load(fd)
 print(data)
 fd.close()
+
+data["population"] = 9600000
+data["gu"] = 25
+print(data)
+
+json_str = json.dumps(data)
+print(json_str)
